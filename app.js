d77d6aa3f8ac0e6c8589ec44e0cc1020dc0575d9
josemrp <josemanuelromerop@gmail.com>
2019-02-02 02:53:36 -0400

    Integrate frontend with the api: - product: GET and POST

--- app.js
+++ app.js
@@ -1,20 +1,77 @@
 $(document).ready(() => {window['APP'] = new Vue({
   el: '#app',
   data: {
-    products: []
+    products: [],
+    newProduct: {
+      name: null,
+      type: null,
+      quantity: null,
+      price: null
+    },
+    typeAvailable: ['protein','carbohydrate','grease','vitamin']
   },
   methods: {
+    // HTTP methods
+    addProduct() {
+      const data = this.$data.newProduct;
+      $.ajax({
+        url: './api/product.php',
+        dataType: 'json',
+        method: 'POST',
+        data
+      }).done(r => {
+        if(!r.error) {
+          const product = {
+            id: r.product_id,
+            name: data.name,
+            type: data.type,
+            quantity: data.quantity
+          };
+          APP.$data.products[product.id] = product;
+        } else {
+          console.error(r)
+        }
+      })
+    },
+    // Useful for the view
+    /**
+     * Hide all collapse and show the selected row
+     * @param {Number} productId 
+     */
+    toggleCollapse(productId) {
+      $('.collapse').collapse('hide');
+      $('#product-detail-'+productId).collapse('toggle');
+      // HTTP getOne
+    },
+    /**
+     * Add attributes for the view in the product list
+     */
+    addAttributes() {
+      const products = this.$data.products;
+      for (const key in products) {
+        if (products.hasOwnProperty(key)) {
+          const product = products[key];
+          product.isCollapse = false;
+          delete product.price_index;
+        }
+      }
+    }
 
   },
+  /**
+   * HTTP getAll
+   */
   created() {
     $.ajax({
       url: './api/product.php',
       dataType: 'json',
     }).done(r => {
-      if(!r.error)
+      if(!r.error) {
         APP.$data.products = r;
-      else
-        console.error(r);
+        APP.addAttributes();
+      } else {
+        console.error(r)
+      }
     })
   }
 })})
