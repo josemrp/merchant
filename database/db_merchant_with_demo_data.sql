bcdbb158114eb86fe26333fa48da71edfba9fdc2
josemrp <josemanuelromerop@gmail.com>
2019-02-05 16:41:11 -0400

    Database

--- database/db_merchant_with_demo_data.sql
+++ database/db_merchant_with_demo_data.sql
@@ -13,6 +13,20 @@ CREATE TABLE `price` (
   `fk_product` int(10) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+INSERT INTO `price` (`id`, `price`, `inserted_at`, `fk_product`) VALUES
+(2, '3.00', '2017-07-06 04:00:00', 63),
+(3, '5.00', '2018-06-19 04:00:00', 63),
+(4, '6.00', '2019-01-30 04:00:00', 63),
+(5, '0.01', '2018-09-02 20:07:03', 68),
+(6, '0.20', '2019-01-23 04:00:00', 68),
+(7, '8.00', '2018-04-11 04:00:00', 65),
+(8, '5.00', '2018-06-12 04:00:00', 65),
+(9, '4.00', '2018-11-07 04:00:00', 65),
+(10, '5.00', '2019-01-31 04:00:00', 65),
+(11, '1.00', '2018-12-30 04:00:00', 69),
+(12, '2.00', '2019-01-30 04:00:00', 69),
+(13, '1.00', '2019-02-01 04:00:00', 66);
+
 CREATE TABLE `product` (
   `id` int(10) UNSIGNED NOT NULL,
   `name` varchar(255) CHARACTER SET latin1 NOT NULL,
@@ -21,6 +35,14 @@ CREATE TABLE `product` (
   `inserted_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `deleted_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+INSERT INTO `product` (`id`, `name`, `quantity`, `type`, `inserted_at`, `deleted_at`) VALUES
+(63, 'Carne (kg)', 0, 'protein', '2019-02-05 19:51:51', NULL),
+(64, 'Cerveza (botella)', 12, NULL, '2019-02-05 19:52:57', NULL),
+(65, 'Cebolla (Kg)', 0.25, 'vitamin', '2019-02-05 19:56:08', NULL),
+(66, 'Juego de mango (L)', 0.75, 'vitamin', '2019-02-05 19:58:58', NULL),
+(68, 'Cafe (g)', 300, NULL, '2019-02-05 20:00:52', NULL),
+(69, 'Mantequilla (mediana)', 1.5, 'grease', '2019-02-05 20:03:34', NULL);
 
 
 ALTER TABLE `price`
@@ -33,10 +55,10 @@ ALTER TABLE `product`
 
 
 ALTER TABLE `price`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 ALTER TABLE `product`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=70;
 
 
 ALTER TABLE `price`
